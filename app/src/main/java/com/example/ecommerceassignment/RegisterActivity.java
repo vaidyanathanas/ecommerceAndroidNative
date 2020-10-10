9d21aa25f7eb5d0f9356e855f1a54f53bb59dfbf
AS <vaidyanathan.as@capgemini.com>
2020-10-10 21:00:35 +0530

    login and register

---
+++ app/src/main/java/com/example/ecommerceassignment/RegisterActivity.java
@@ -0,0 +1,132 @@
+package com.example.ecommerceassignment;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.app.ProgressDialog;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private Button CreateAccount;
+    private EditText InputName, InputNumber, InputPassword;
+    private ProgressDialog loadingBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        CreateAccount = (Button) findViewById(R.id.register_btn);
+        InputName = (EditText) findViewById(R.id.register_name_input);
+        InputNumber = (EditText) findViewById(R.id.register_phone_number_input);
+        InputPassword = (EditText) findViewById(R.id.register_password_input);
+
+        loadingBar = new ProgressDialog(this);
+
+        CreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createAccount();
+            }
+        });
+    }
+
+    private void createAccount(){
+
+      String name =  InputName.getText().toString();
+        String number =InputNumber.getText().toString();
+        String password =    InputPassword.getText().toString();
+        if(TextUtils.isEmpty(name)){
+            Toast.makeText(this,"Please write your name",Toast.LENGTH_SHORT).show();
+
+        } else if(TextUtils.isEmpty(number)){
+            Toast.makeText(this,"Please write your number",Toast.LENGTH_SHORT).show();
+
+        } else if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Please write your password",Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            loadingBar.setTitle("Create Account");
+            loadingBar.setMessage("Please wait, while we are checking the credentials.");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+            ValidatephoneNumber(name, number, password);
+        }
+
+    }
+
+    private void ValidatephoneNumber(final String name, final String phone, final String password)
+    {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                if (!(dataSnapshot.child("Users").child(phone).exists()))
+                {
+                    HashMap<String, Object> userdataMap = new HashMap<>();
+                    userdataMap.put("phone", phone);
+                    userdataMap.put("password", password);
+                    userdataMap.put("name", name);
+
+                    RootRef.child("Users").child(phone).updateChildren(userdataMap)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task)
+                                {
+                                    if (task.isSuccessful())
+                                    {
+                                        Toast.makeText(RegisterActivity.this, "Congratulations, your account has been created.", Toast.LENGTH_SHORT).show();
+                                        loadingBar.dismiss();
+
+                                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                        startActivity(intent);
+                                    }
+                                    else
+                                    {
+                                        loadingBar.dismiss();
+                                        Toast.makeText(RegisterActivity.this, "Network Error: Please try again after some time...", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+                else
+                {
+                    Toast.makeText(RegisterActivity.this, "This " + phone + " already exists.", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                    Toast.makeText(RegisterActivity.this, "Please try again using another phone number.", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+}
